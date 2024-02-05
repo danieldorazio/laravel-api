caaf75a681a644d1c12f1746c9c6a9a51bf3340e
danieldorazio <danieldorazio.97@gmail.com>
2024-02-05 19:04:22 +0100

    improved api/show call and user function in Model Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -29,7 +29,7 @@ class Project extends Model
         return $this->belongsToMany(Tecnology::class);
     }
 
-    public function users() {
+    public function user() {
         return $this->belongsTo(User::class);
     }
 }
